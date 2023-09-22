4b520b7f5f8066121cfe03996d8ccdb650850c51
KoshidaManato <k022g0129@g.neec.ac.jp>
2023-09-22 16:07:19 +0900

    あああ

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -22,3 +22,7 @@ void TitleScene::Update(){
 };
 
 void TitleScene::Draw() { Title_ = Sprite::Create(Title_, {0, 0}, {0, 0, 0, 0}, {0, 0}); };
+
+// スプライト生成
+sprite2DReticle_ =
+    Sprite::Create(textureReticle, {640.0f, 360.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.5f, 0.5f});
